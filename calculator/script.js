1561a3d0aed5f2a3f0e70765a5020f3ced9c7dcb
Douglas Nakashima <douglas.naka@gmail.com>
2018-03-02 06:12:41 +0900

    Added functionality to the number buttons

--- calculator/script.js
+++ calculator/script.js
@@ -10,16 +10,23 @@ const operate = (op, x, y) => {
         : multiply(x, y) 
 };
 
-let numbers;
 
 const container = document.querySelector('#container');
+const numbers = [];
+const getNumbers = document.querySelector('#numbers');
+
 
 const createNumbers = () => {
     for (let i = 0; i < 10; i++) {
         const buttons = document.createElement('button');
-        buttons.setAttribute('id', 'numbers');
+        buttons.setAttribute('id', i);
         buttons.innerHTML = i;
         container.appendChild(buttons);
+        buttons.addEventListener('click', (e) => {
+            console.log(buttons.innerHTML);
+            numbers.push(i);
+            getNumbers.innerHTML = numbers;
+        })
     }
 }
 createNumbers();
