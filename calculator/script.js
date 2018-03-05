4b7b2749028b1212d0879f8fd0de039237aedb9e
Douglas Nakashima <douglas.naka@gmail.com>
2018-03-05 17:49:25 +0900

    Made the addition button work, still needs to make it display the result

--- calculator/script.js
+++ calculator/script.js
@@ -4,31 +4,53 @@ const divide = (x, y) => x / y;
 const multiply = (x, y) => x * y;
 
 const operate = (op, x, y) => {
-    return op === add ? add(x, y) 
-        : op === subtract ? subtract(x, y) 
-        : op === divide ? divide(x, y) 
-        : multiply(x, y) 
+    return op(x, y);
 };
 
-
 const container = document.querySelector('#container');
-const numbers = [];
+let display = '';
+let curNum;
+let newNum;
 const getNumbers = document.querySelector('#numbers');
-
 
 const createNumbers = () => {
     for (let i = 0; i < 10; i++) {
         const buttons = document.createElement('button');
-        buttons.setAttribute('id', i);
+        buttons.setAttribute('id', 'nums');
         buttons.innerHTML = i;
         container.appendChild(buttons);
+        
         buttons.addEventListener('click', (e) => {
-            console.log(buttons.innerHTML);
-            numbers.push(i);
-            getNumbers.innerHTML = numbers;
+            display += i;
+            getNumbers.innerHTML = display;
         })
-    }
+    }   
 }
 createNumbers();
 
-console.log(operate(multiply, 2, 2));
+console.log(createNumbers.buttons);
+
+// const newNumbers = () => {
+//     const buttons = document.querySelectorAll('nums');
+//     buttons.forEach(button => {
+//         buttons.addEventListener('click', (e) => {
+//             numbers += buttons.innerHTML;
+//             getNumbers.innerHTML = numbers;
+//         })
+//     });  
+// }
+// newNumbers();
+
+const addOp = document.querySelector('#add');
+addOp.addEventListener('click', (e) => {
+    if (curNum === undefined) {
+        curNum = Number(display);
+        display = '';
+        console.log(curNum)   
+    } else if (newNum === undefined) {
+        newNum = Number(display);
+        display = '';
+        curNum = operate(add, curNum, newNum);
+        newNum = undefined;
+    };     
+})
